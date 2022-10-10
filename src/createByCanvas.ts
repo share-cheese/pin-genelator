2ab8fa856ee49720ec84f91359813a7e369d5358
Yuta Ohira <alesion90@gmail.com>
2022-10-10 18:12:51 +0900

    fix

--- src/createByCanvas.ts
+++ src/createByCanvas.ts
@@ -3,6 +3,7 @@ import { createCanvas, loadImage } from "canvas";
 import fs from "fs";
 
 const CANVAS_WIDTH = 200;
+const CANVAS_HEIGHT = CANVAS_WIDTH * 1.4;
 
 const run = async () => {
   await fs.promises.rm("out", { recursive: true, force: true });
@@ -12,18 +13,18 @@ const run = async () => {
 
   bases.forEach((base) => {
     items.forEach(async (item) => {
-      const canvas = createCanvas(CANVAS_WIDTH, CANVAS_WIDTH * 1.4, "svg");
+      const canvas = createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT, "svg");
       const ctx = canvas.getContext("2d");
 
       const baseSvg = await loadImage(`src/img/base/${base}`);
       const itemSvg = await loadImage(`src/img/item/${item}`);
-      ctx.drawImage(baseSvg, 0, 0, CANVAS_WIDTH, CANVAS_WIDTH * 1.4);
+      ctx.drawImage(baseSvg, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
       const itemWidth = (CANVAS_WIDTH / 100) * 48;
       ctx.drawImage(
         itemSvg,
         (CANVAS_WIDTH - itemWidth) / 2,
-        (CANVAS_WIDTH * 1.4 - itemWidth) / 2 - 24,
+        (CANVAS_HEIGHT - itemWidth / 2) / 4,
         itemWidth,
         itemWidth
       );
